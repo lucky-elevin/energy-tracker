dd1d1fd3b477db4c5f0136305372f32ca14e41d2
Yauheni Levin <yauheni@MacBook-Pro-Yauheni-2.local>
2026-05-10 22:45:29 +0200

    ET-37 Setup Crashlytics for iOS and Android

--- Android/Kaizen/app/build.gradle.kts
+++ Android/Kaizen/app/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
 }
 
 android {
-    namespace = "com.example.kaizen"
+    namespace = "com.yauhenilevin.kaizen"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
@@ -12,7 +14,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.kaizen"
+        applicationId = "com.yauhenilevin.kaizen"
         minSdk = 35
         targetSdk = 36
         versionCode = 1
@@ -28,6 +30,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            applicationIdSuffix = ".staging"
+            versionNameSuffix = "-DEBUG"
         }
     }
     compileOptions {
@@ -48,6 +54,11 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Android/Kaizen/app/src/main/java/com/yauhenilevin/kaizen/ui/theme/Type.kt
+++ Android/Kaizen/app/src/main/java/com/yauhenilevin/kaizen/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.example.kaizen.ui.theme
+package com.yauhenilevin.kaizen.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
